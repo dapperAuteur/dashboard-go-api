413a0dc04b4427ca694b33cf24ec6093894d051d
awe <a@awews.com>
2020-08-04 16:37:36 -0700

    graceful server shutdown

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 
 	// ===
+	// App Starting
 
 	log.Printf("main : Started")
 	defer log.Println("main : Completed")
@@ -21,7 +22,45 @@ func main() {
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	}
+
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Printf("main : API listening on %s", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	// ==
+	// Shutdown
+
+	// Blocking main and waiting for shutdown.
+	select {
+	case err := <-serverErrors:
 		log.Fatalf("error: listening and serving: %s", err)
+
+	case <-shutdown:
+		log.Println("main : Start shutdown")
+
+		// Give outstanding requests a deadline for completion.
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		// Asking listener to shutdown and load shed.
+		err := api.Shutdown(ctx)
+		if err != nil {
+			log.Printf("main : Graceful shutdown did not complete in %v : %v", timeout, err)
+			err = api.Close()
+		}
+
+		if err != nil {
+			log.Fatalf("main : could not stop server gracefully : %v", err)
+		}
 	}
 }
 
