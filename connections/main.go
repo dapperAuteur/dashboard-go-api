b06d989718930973da3fa29aa30872999d7a93ee
awe <a@awews.com>
2020-08-05 16:17:00 -0700

    print db names to terminal

--- connections/main.go
+++ connections/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -30,4 +32,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// print the names of the dbs to terminal
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
 }
