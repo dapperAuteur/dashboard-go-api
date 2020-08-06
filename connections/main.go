fc17d277e260d30926a4b9719c4c9306a47d0db4
awe <a@awews.com>
2020-08-05 17:11:20 -0700

    prepare for learning how to query mongo db with go

--- connections/main.go
+++ connections/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -31,33 +29,5 @@ func main() {
 	quickstartDatabase := client.Database("quickstart")
 	podcastsCollection := quickstartDatabase.Collection("podcasts")
 	episodesCollection := quickstartDatabase.Collection("episodes")
-	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
-		{"title", "The Polyglot Developer Podcast"},
-		{"author", "Nic Raboy"},
-		{"tags", bson.A{"development", "programming", "coding"}},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(podcastResult.InsertedID)
 
-	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
-		bson.D{
-			{"podcast", podcastResult.InsertedID},
-			{"title", "Episode #1"},
-			{"description", "This is the first episode"},
-			{"duration", 25},
-		},
-		bson.D{
-			{"podcast", podcastResult.InsertedID},
-			{"title", "Episode #2"},
-			{"description", "This is the second episode"},
-			{"duration", 55},
-		},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(episodeResult.InsertedIDs)
-	fmt.Println(*episodeResult)
 }
