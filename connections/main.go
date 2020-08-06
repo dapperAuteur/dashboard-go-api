5f291d95f65a2b7c862f66a325e724802a0fd0db
awe <a@awews.com>
2020-08-05 21:15:36 -0700

    another way to connect and query the db
    wasn't able to get it to work with environment variables

--- connections/main.go
+++ connections/main.go
@@ -3,67 +3,50 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	// connect to db
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
 	// this formats the client
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://awe:XjtsRQPAjyDbokQE@palabras-express-api.whbeh.mongodb.net/palabras-express-api?retryWrites=true&w=majority"))
+	// client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("ATLAS_URI")))
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://awe:XjtsRQPAjyDbokQE@palabras-express-api.whbeh.mongodb.net/palabras-express-api?retryWrites=true&w=majority"))
 
 	// if there's an error do log the error
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	// connect to db
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer client.Disconnect(ctx)
 
 	database := client.Database(("quickstart"))
 	podcastsCollection := database.Collection("podcasts")
-	// 5f2b44a5f15bb82f27e4c0f3
+	episodesCollection := database.Collection("episodes")
 
-	// convert the id to something mongo go driver understand
-	id, _ := primitive.ObjectIDFromHex("5f2b44a5f15bb82f27e4c0f3")
-
-	result, err := podcastsCollection.UpdateOne(
-		ctx,
-		bson.M{"_id": id},
-		bson.D{
-			{"$set", bson.D{{"author", "Nicolas Raboy"}}},
-		},
-	)
+	var podcasts []bson.M
+	podcastCursor, err := podcastsCollection.Find(ctx, bson.M{})
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
 
-	result, err = podcastsCollection.UpdateMany(
-		ctx,
-		bson.M{"title": "The Polyglot Developer Podcast"},
-		bson.D{
-			{"$set", bson.D{{"author", "Nic Raboy"}}},
-		},
-	)
-	fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
+	if err = podcastCursor.All(ctx, &podcasts); err != nil {
+		panic(err)
+	}
+	fmt.Println(podcasts)
 
-	result, err = podcastsCollection.ReplaceOne(
-		ctx,
-		bson.M{"author": "Nic Raboy"},
-		bson.M{
-			"title":  "The Nic Raboy Show",
-			"author": "Nicolas Raboy",
-		},
-	)
-	fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
+	var episodes []bson.M
+	episodeCursor, err := episodesCollection.Find(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+	if err = episodeCursor.All(ctx, &episodes); err != nil {
+		panic(err)
+	}
+	fmt.Println(episodes)
 }
