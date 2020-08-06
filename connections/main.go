7f0b9ad7c932ed9432ff2d016f79a052d1001c2a
awe <a@awews.com>
2020-08-05 18:26:22 -0700

    update 1 document by id

--- connections/main.go
+++ connections/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -25,5 +28,24 @@ func main() {
 		log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
+
+	database := client.Database(("quickstart"))
+	podcastsCollection := database.Collection("podcasts")
+	// 5f2b44a5f15bb82f27e4c0f3
+
+	// convert the id to something mongo go driver understand
+	id, _ := primitive.ObjectIDFromHex("5f2b44a5f15bb82f27e4c0f3")
+
+	result, err := podcastsCollection.UpdateOne(
+		ctx,
+		bson.M{"_id": id},
+		bson.D{
+			{"$set", bson.D{{"author", "Nicolas Raboy"}}},
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
 
 }
