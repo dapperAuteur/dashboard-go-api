dec71899fd95e6bce5047af3c357a592735f2204
awe <a@awews.com>
2020-08-05 18:18:08 -0700

    prepare to learn how to update documents

--- connections/main.go
+++ connections/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -28,58 +26,4 @@ func main() {
 	}
 	defer client.Disconnect(ctx)
 
-	quickstartDatabase := client.Database("quickstart")
-	podcastsCollection := quickstartDatabase.Collection("podcasts")
-	episodesCollection := quickstartDatabase.Collection("episodes")
-
-	cursor, err := episodesCollection.Find(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	// var episodes []bson.M
-	// if err = cursor.All(ctx, &episodes); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// for _, episode := range episodes {
-	// 	fmt.Println(episode["title"])
-	// }
-
-	defer cursor.Close(ctx)
-	for cursor.Next(ctx) {
-		var episode bson.M
-		if err = cursor.Decode(&episode); err != nil {
-			log.Fatal(err)
-		}
-		// fmt.Println(episode)
-	}
-
-	var podcast bson.M
-	if err = podcastsCollection.FindOne(ctx, bson.M{}).Decode(&podcast); err != nil {
-		log.Fatal(err)
-	}
-	// fmt.Println("podcast", podcast)
-
-	filterCursor, err := episodesCollection.Find(ctx, bson.M{"duration": 25})
-	if err != nil {
-		log.Fatal(err)
-	}
-	var episodesFiltered []bson.M
-	if err = filterCursor.All(ctx, &episodesFiltered); err != nil {
-		log.Fatal(err)
-	}
-	// fmt.Println(episodesFiltered)
-
-	opts := options.Find()
-	opts.SetSort(bson.D{{"duration", 1}})
-
-	sortCursor, err := episodesCollection.Find(ctx, bson.D{
-		{"duration", bson.D{
-			{"$gt", 24},
-		}},
-	}, opts)
-	var episodesSorted []bson.M
-	if err = sortCursor.All(ctx, &episodesSorted); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(episodesSorted)
 }
