47fcea3cdd4ff6c0f793c3badeafabe16d016571
awe <a@awews.com>
2020-08-05 18:35:26 -0700

    update many documents on one request
    find document by property not id

--- connections/main.go
+++ connections/main.go
@@ -40,12 +40,21 @@ func main() {
 		ctx,
 		bson.M{"_id": id},
 		bson.D{
-			{"$set", bson.D{{"author", "Nicola Raboy"}, {"favorite", false}, {"mine", false}}},
+			{"$set", bson.D{{"author", "Nicolas Raboy"}}},
 		},
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
+
+	result, err = podcastsCollection.UpdateMany(
+		ctx,
+		bson.M{"title": "The Polyglot Developer Podcast"},
+		bson.D{
+			{"$set", bson.D{{"author", "Nic Raboy"}}},
+		},
+	)
 	fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
 
 }
