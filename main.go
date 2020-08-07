b182d6af4ee18a84a1d6e61ef935bf0dd6b7301a
awe <a@awews.com>
2020-08-07 16:13:57 -0700

    comment out all code not needed to connect to mongodb

--- main.go
+++ main.go
@@ -136,19 +136,69 @@ func ListTransactions(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func ListVerbos(w http.ResponseWriter, r *http.Request) {
-	verbos := []Verbo{}
+// // Transaction is a line item on a balance sheet.
+// type Transaction struct {
+// 	Budget           string  `json:"budget,omitempty"`
+// 	Currency         string  `json:"currency,omitempty"`
+// 	FinancialAccount string  `json:"financial_account,omitempty"`
+// 	Media            string  `json:"media,omitempty"`
+// 	Note             string  `json:"note,omitempty"`
+// 	Occurrence       string  `json:"occurrence,omitempty"`
+// 	Participant      string  `json:"participant,omitempty"`
+// 	Tag              string  `json:"tag,omitempty"`
+// 	TransactionEvent string  `json:"transaction_event,omitempty"`
+// 	TransactionValue float64 `json:"transaction_value,omitempty"`
+// 	Vendor           string  `json:"vendor,omitempty"`
+// }
 
-	data, err := json.Marshal(verbos)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println("error marshalling", err)
-		return
-	}
+// // Verbo is a Spanish verb
+// type Verbo struct {
+// 	CambiarDeIrregular   string  `json:"cambiar_de_irregular,omitempty"`
+// 	CategoriaDeIrregular string  `json:"categoria_de_irregular,omitempty"`
+// 	English              string  `json:"english,omitempty"`
+// 	Grupo                float64 `json:"grupo,omitempty"`
+// 	Irregular            bool    `json:"irregular,omitempty"`
+// 	Media                string  `json:"media,omitempty"`
+// 	Note                 string  `json:"note,,omitempty"`
+// 	Reflexive            bool    `json:"reflexive,omitempty"`
+// 	Spanish              string  `json:"spanish,omitempty"`
+// 	Terminacion          string  `json:"terminacion,omitempty"`
+// }
 
-	w.Header().Set("content-type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write(data); err != nil {
-		log.Println("error writing", err)
-	}
-}
+// // ListTransactions is an HTTP Handler for returning a list of Transactions.
+// func ListTransactions(w http.ResponseWriter, r *http.Request) {
+// 	list := []Transaction{
+// 		{Budget: "Food", TransactionValue: 14.39, Vendor: "Fry's"},
+// 		{Budget: "Tools", TransactionValue: 1400.39, Vendor: "System76"},
+// 	}
+
+// 	data, err := json.Marshal(list)
+// 	if err != nil {
+// 		log.Println("error marshalling result", err)
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+// 	w.WriteHeader(http.StatusOK)
+// 	if _, err := w.Write(data); err != nil {
+// 		log.Println("error writing result", err)
+// 	}
+// }
+
+// func ListVerbos(w http.ResponseWriter, r *http.Request) {
+// 	verbos := []Verbo{}
+
+// 	data, err := json.Marshal(verbos)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 		log.Println("error marshalling", err)
+// 		return
+// 	}
+
+// 	w.Header().Set("content-type", "application/json; charset=utf-8")
+// 	w.WriteHeader(http.StatusOK)
+// 	if _, err := w.Write(data); err != nil {
+// 		log.Println("error writing", err)
+// 	}
+// }
