f9f5b7587d926bf103eda11003df88912a28835e
awe <a@awews.com>
2020-08-07 12:50:05 -0700

    start refactoring to use mongo

--- main.go
+++ main.go
@@ -24,15 +24,9 @@ func main() {
 
 	// ==
 	// Start Database
-
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://awe:XjtsRQPAjyDbokQE@palabras-express-api-shard-00-00.whbeh.mongodb.net:27017,palabras-express-api-shard-00-01.whbeh.mongodb.net:27017,palabras-express-api-shard-00-02.whbeh.mongodb.net:27017/palabras-express-api?ssl=true&replicaSet=atlas-12h93v-shard-0&authSource=admin&retryWrites=true&w=majority"))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://awe:XjtsRQPAjyDbokQE@palabras-express-api.whbeh.mongodb.net/palabras-express-api?retryWrites=true&w=majority"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -94,17 +88,31 @@ func main() {
 
 // Transaction is a line item on a balance sheet.
 type Transaction struct {
-	Budget           string  `json:"budget"`
-	Currency         string  `json:"currency"`
-	FinancialAccount string  `json:"financial_account"`
-	Media            string  `json:"media"`
-	Note             string  `json:"note"`
-	Occurrence       string  `json:"occurrence"`
-	Participant      string  `json:"participant"`
-	Tag              string  `json:"tag"`
-	TransactionEvent string  `json:"transaction_event"`
-	TransactionValue float64 `json:"transaction_value"`
-	Vendor           string  `json:"vendor"`
+	Budget           string  `json:"budget,omitempty"`
+	Currency         string  `json:"currency,omitempty"`
+	FinancialAccount string  `json:"financial_account,omitempty"`
+	Media            string  `json:"media,omitempty"`
+	Note             string  `json:"note,omitempty"`
+	Occurrence       string  `json:"occurrence,omitempty"`
+	Participant      string  `json:"participant,omitempty"`
+	Tag              string  `json:"tag,omitempty"`
+	TransactionEvent string  `json:"transaction_event,omitempty"`
+	TransactionValue float64 `json:"transaction_value,omitempty"`
+	Vendor           string  `json:"vendor,omitempty"`
+}
+
+// Verbo is a Spanish verb
+type Verbo struct {
+	CambiarDeIrregular   string  `json:"cambiar_de_irregular,omitempty"`
+	CategoriaDeIrregular string  `json:"categoria_de_irregular,omitempty"`
+	English              string  `json:"english,omitempty"`
+	Grupo                float64 `json:"grupo,omitempty"`
+	Irregular            bool    `json:"irregular,omitempty"`
+	Media                string  `json:"media,omitempty"`
+	Note                 string  `json:"note,,omitempty"`
+	Reflexive            bool    `json:"reflexive,omitempty"`
+	Spanish              string  `json:"spanish,omitempty"`
+	Terminacion          string  `json:"terminacion,omitempty"`
 }
 
 // ListTransactions is an HTTP Handler for returning a list of Transactions.
@@ -125,5 +133,22 @@ func ListTransactions(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	if _, err := w.Write(data); err != nil {
 		log.Println("error writing result", err)
+	}
+}
+
+func ListVerbos(w http.ResponseWriter, r *http.Request) {
+	verbos := []Verbo{}
+
+	data, err := json.Marshal(verbos)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("error marshalling", err)
+		return
+	}
+
+	w.Header().Set("content-type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		log.Println("error writing", err)
 	}
 }
