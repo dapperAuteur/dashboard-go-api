c460157546e8cdcc912d8f2216fc2b0fae6c4c29
awe <a@awews.com>
2020-08-07 17:32:01 -0700

    refactor to move database connection to internal directory
    this prevents others form importing the package and possibly getting breaking changes

---
+++ internal/platform/database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Open knows how to open a database connection
+func Open() (*mongo.Client, error) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	// formats the client
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://awe:XjtsRQPAjyDbokQE@palabras-express-api.whbeh.mongodb.net/palabras-express-api?retryWrites=true&w=majority"))
+
+	return client, err
+}
