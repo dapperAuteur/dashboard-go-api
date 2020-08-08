3a6cde4121322d58baf1b7cafffcd449253fcb37
awe <a@awews.com>
2020-08-08 10:23:01 -0700

    refactor admin to use manage fatal errors, config var, & add db config vals

--- cmd/dashboard-admin/main.go
+++ cmd/dashboard-admin/main.go
@@ -2,22 +2,63 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"os"
 	"time"
 
+	"github.com/dapperAuteur/dashboard-go-api/internal/platform/conf"
 	"github.com/dapperAuteur/dashboard-go-api/internal/platform/database"
+	"github.com/pkg/errors"
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.Printf("error: shutting down: %s", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+
+	// ==
+	// Configuration
+
+	var cfg struct {
+		DB struct {
+			AtlasUri string `conf:"default:mongodb+srv://awe:XjtsRQPAjyDbokQE@palabras-express-api.whbeh.mongodb.net/palabras-express-api?retryWrites=true&w=majority"`
+		}
+	}
+
+	// ==
+	// Get Configuration
+	// Helpful info in case of error
+
+	if err := conf.Parse(os.Args[1:], "DASHBOARD", &cfg); err != nil {
+		if err == conf.ErrHelpWanted {
+			usage, err := conf.Usage("DASHBOARD", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
 
 	// ==
 	// Start Database
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-	client, err := database.Open()
+	client, err := database.Open(database.Config{
+		AtlasUri: cfg.DB.AtlasUri,
+	})
 	if err != nil {
-		panic(err)
+		return errors.Wrap(err, "connecting to db")
 	}
 	defer client.Disconnect(ctx)
+
+	return nil
 
 }
 
