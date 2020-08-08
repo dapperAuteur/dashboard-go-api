bfca7e8562212e9f219c4b60060cd48a34a347f7
awe <a@awews.com>
2020-08-08 13:02:11 -0700

    create retrieve handler to marshal data from router to db

--- cmd/dashboard-api/internal/handlers/podcast.go
+++ cmd/dashboard-api/internal/handlers/podcast.go
@@ -51,3 +51,38 @@ func (p Podcast) PodcastList(w http.ResponseWriter, r *http.Request) {
 		p.Log.Println("error writing result", err)
 	}
 }
+
+// Retrieve gets the Podcast from the db by _id then encodes them in a response client
+func (p Podcast) Retrieve(w http.ResponseWriter, r *http.Request) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	_id := "_id"
+
+	podcast, err := podcast.Retrieve(p.DB, _id)
+	if err != nil {
+		panic(err)
+	}
+
+	podcastCursor, err := p.DB.Find(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err = podcastCursor.All(ctx, &podcast); err != nil {
+		panic(err)
+	}
+	fmt.Println(podcast)
+
+	data, err := json.Marshal(podcast)
+	if err != nil {
+		p.Log.Println("error marshalling result", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		p.Log.Println("error writing result", err)
+	}
+}
