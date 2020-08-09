e28fc131f1de5f6f3c880891b2101d81c6dfbbad
awe <a@awews.com>
2020-08-09 16:40:32 -0700

    retrieve  gets the first Podcast in the db with the provided _id

--- internal/podcast/podcast.go
+++ internal/podcast/podcast.go
@@ -2,9 +2,12 @@ package podcast
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive" // for BSON ObjectID
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -25,24 +28,27 @@ func List(db *mongo.Collection) ([]Podcast, error) {
 	return podcastList, nil
 }
 
+// Retrieve gets the first Podcast in the db with the provided _id
 func Retrieve(db *mongo.Collection, _id string) (*Podcast, error) {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	var podcast Podcast
 
-	podcastFound := bson.M{"ID": _id}
+	id, err := primitive.ObjectIDFromHex(_id)
 	err := db.FindOne(ctx, podcastFound).Decode(&podcast)
 	if err != nil {
 		return nil, err
 	}
 
-	// podcastCursor, err := db.FindOne(ctx, Podcast{ID: _id})
-	// if err != nil {
-	// 	panic(err)
-	// }
+	if err := db.FindOne(ctx, bson.M{"_id": id}).Decode(&podcast); err != nil {
+		log.Printf("podcast not found: %s", podcast)
+		log.Printf("id sent to podcast.Retrieve podcast}: %s", podcast)
+		return nil, err
+	}
 
-	// if err = podcastCursor.All(ctx, &podcast); err != nil {
-	// 	return nil, err
-	// }
+	fmt.Println("result AFTER:", podcast)
+
+	return &podcast, nil
+}
 
 	return &podcast, nil
 }
