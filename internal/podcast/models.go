241e9428a13224a0e9943a414948402681f2cdf1
awe <a@awews.com>
2020-08-10 11:23:44 -0700

    add subscribers to Podcast and spins to Episode

--- internal/podcast/models.go
+++ internal/podcast/models.go
@@ -8,21 +8,23 @@ import (
 
 // Podcast type is a group of related episodes
 type Podcast struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Title     string             `bson:"title,omitempty" json:"title,omitempty"`
-	Author    string             `bson:"author,omitempty" json:"author,omitempty"`
-	Tags      []string           `bson:"tags,omitempty" json:"tags,omitempty"`
-	Published bool               `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
-	CreatedAt time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
-	UpdatedAt time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Title       string             `bson:"title,omitempty" json:"title,omitempty"`
+	Author      string             `bson:"author,omitempty" json:"author,omitempty"`
+	Subscribers int                `bson:"subscribers,omitempty,default:0" json:"subscribers,omitempty,default:0`
+	Tags        []string           `bson:"tags,omitempty" json:"tags,omitempty"`
+	Published   bool               `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
+	CreatedAt   time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UpdatedAt   time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
 }
 
 // NewPodcast type is what's required from client to create a new Podcast
 type NewPodcast struct {
-	Title     string   `bson:"_id,omitempty" json:"_id,omitempty"`
-	Author    string   `bson:"title,omitempty" json:"title,omitempty"`
-	Tags      []string `bson:"tags,omitempty" json:"tags,omitempty"`
-	Published bool     `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
+	Title       string   `bson:"_id,omitempty" json:"_id,omitempty"`
+	Author      string   `bson:"title,omitempty" json:"title,omitempty"`
+	Subscribers int      `bson:"subscribers,omitempty,default:0" json:"subscribers,omitempty,default:0`
+	Tags        []string `bson:"tags,omitempty" json:"tags,omitempty"`
+	Published   bool     `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
 }
 
 // Episode is the video or audio content
@@ -32,6 +34,7 @@ type Episode struct {
 	Title       string             `bson:"title,omitempty" json:"title,omitempty"`
 	Description string             `bson:"description,omitempty" json:"description,omitempty"`
 	Duration    int32              `bson:"duration,omitempty" json:"duration,omitempty"`
+	Spins       int                `bson:"spins,omitempty,default:0" json:"spins,omitempty,default:0`
 	Tags        []string           `bson:"tags,omitempty" json:"tags,omitempty"`
 	Published   bool               `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
 	CreatedAt   time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
@@ -44,6 +47,7 @@ type NewEpisode struct {
 	Title       string             `bson:"title,omitempty" json:"title,omitempty"`
 	Description string             `bson:"description,omitempty" json:"description,omitempty"`
 	Duration    int32              `bson:"duration,omitempty" json:"duration,omitempty"`
+	Spins       int                `bson:"spins,omitempty,default:0" json:"spins,omitempty,default:0`
 	Published   bool               `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
 	Tags        []string           `bson:"tags,omitempty" json:"tags,omitempty"`
 }
