f50b7656ba2104a7b07b06c04ee64d462b4beb1f
awe <a@awews.com>
2020-08-10 12:37:59 -0700

    add function to Add Episode to db

--- internal/podcast/episode.go
+++ internal/podcast/episode.go
@@ -13,20 +13,30 @@ import (
 // AddEpisode adds an Episode to a Podcast
 func AddEpisode(ctx context.Context, db *mongo.Collection, newEpisode NewEpisode, podcastID string, now time.Time) (*Episode, error) {
 
+	// convert podcastID string from url var to podcastObjectID
 	podcastObjectID, err := primitive.ObjectIDFromHex(podcastID)
 	if err != nil {
 		return nil, errors.Wrapf(err, "converting string to ObjectID")
 	}
 
-	episode := NewEpisode{
+	// put provided values into NewPodcast struct
+	episode := Episode{
 		PodcastID:   podcastObjectID,
 		Title:       newEpisode.Title,
 		Description: newEpisode.Description,
 		Duration:    newEpisode.Duration,
+		Spins:       newEpisode.Spins,
+		Published:   newEpisode.Published,
 		Tags:        newEpisode.Tags,
+		CreatedAt:   now.UTC(),
+		UpdatedAt:   now.UTC(),
 	}
 
-	fmt.Println("newEpisode : " epispode),
-	// use db connection to insertOne
-	return &episode
+	episodeResult, err := db.InsertOne(ctx, episode)
+	if err != nil {
+		return nil, errors.Wrapf(err, "inserting Episode: %v", newEpisode)
+	}
+	fmt.Println("episodeReult : ", episodeResult)
+
+	return &episode, nil
 }
