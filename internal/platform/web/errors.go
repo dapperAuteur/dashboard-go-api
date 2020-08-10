4240153c1304331f30f9af0084ea5048e1035429
awe <a@awews.com>
2020-08-09 20:29:40 -0700

    create new error struct to handle request errors

--- internal/platform/web/errors.go
+++ internal/platform/web/errors.go
@@ -4,3 +4,18 @@ package web
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
+
+// Error is used to add web information to request error.
+type Error struct {
+	Err    error
+	Status int
+}
+
+// NewRequestError is used when a known error condition is encountered.
+func NewRequestError(err error, status int) error {
+	return &Error{Err: err, Status: status}
+}
+
+func (e *Error) Error() string {
+	return e.Err.Error()
+}
