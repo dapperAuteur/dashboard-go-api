feb708a0a47a8ea7c60220d12517c94c3792c275
awe <a@awews.com>
2020-08-10 12:37:32 -0700

    create AddEpisode Handler

--- cmd/dashboard-api/internal/handlers/episode.go
+++ cmd/dashboard-api/internal/handlers/episode.go
@@ -3,7 +3,11 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/dapperAuteur/dashboard-go-api/internal/platform/web"
+	"github.com/dapperAuteur/dashboard-go-api/internal/podcast"
+	"github.com/go-chi/chi"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -15,7 +19,7 @@ type Episode struct {
 
 // EpisodeList gets all the Episodes from the db of a specific Podcast.
 // Then encodes them in a response client.
-func (episode Episode) EpisodeList(w http.ResponseWriter, r *http.Request) error {
+func (e Episode) EpisodeList(w http.ResponseWriter, r *http.Request) error {
 
 	// episodeList, err := episode.EpisodeList(r.Context(), e.DB)
 	// if err != nil {
@@ -26,7 +30,20 @@ func (episode Episode) EpisodeList(w http.ResponseWriter, r *http.Request) error
 
 // AddEpisode decodes a JSON document from a POST request and creates a new Episode for a specific Podcast.
 // BUG: Will create empty object!!! Validate content before accepting
-func (e Episode) AddEpisode(w http.ResponseWriter, r *http.Request) {
+func (e Episode) AddEpisode(w http.ResponseWriter, r *http.Request) error {
 
-	// var newEpisode episode.Ne
+	var newEpisode podcast.NewEpisode
+
+	podcastID := chi.URLParam(r, "_id")
+
+	if err := web.Decode(r, &newEpisode); err != nil {
+		return err
+	}
+
+	episode, err := podcast.AddEpisode(r.Context(), e.DB, newEpisode, podcastID, time.Now())
+	if err != nil {
+		return err
+	}
+
+	return web.Respond(w, episode, http.StatusCreated)
 }
