d67e8e596e2cdd221e44e208a15eb95f5c3fd7d0
awe <a@awews.com>
2020-08-10 15:01:17 -0700

    refactor structs to fix names, ensure proper validation

--- internal/podcast/models.go
+++ internal/podcast/models.go
@@ -9,9 +9,9 @@ import (
 // Podcast type is a group of related episodes
 type Podcast struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Title       string             `bson:"title,omitempty" json:"title,omitempty"`
-	Author      string             `bson:"author,omitempty" json:"author,omitempty"`
-	Subscribers int                `bson:"subscribers,omitempty,default:0" json:"subscribers,omitempty,default:0`
+	Title       string             `bson:"title,omitempty" json:"title,omitempty" validate:"required"`
+	Author      string             `bson:"author,omitempty" json:"author,omitempty" validate:"required"`
+	Subscribers int                `bson:"subscribers,omitempty,default:0" json:"subscribers,omitempty,default:0" validate:"gte=0"`
 	Tags        []string           `bson:"tags,omitempty" json:"tags,omitempty"`
 	Published   bool               `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
 	CreatedAt   time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
@@ -20,9 +20,9 @@ type Podcast struct {
 
 // NewPodcast type is what's required from client to create a new Podcast
 type NewPodcast struct {
-	Title       string   `bson:"_id,omitempty" json:"_id,omitempty"`
-	Author      string   `bson:"title,omitempty" json:"title,omitempty"`
-	Subscribers int      `bson:"subscribers,omitempty,default:0" json:"subscribers,omitempty,default:0`
+	Title       string   `bson:"title,omitempty" json:"title,omitempty" validate:"required"`
+	Author      string   `bson:"author,omitempty" json:"author,omitempty" validate:"required"`
+	Subscribers int      `bson:"subscribers,omitempty,default:0" json:"subscribers,omitempty,default:0" validate:"gte=0"`
 	Tags        []string `bson:"tags,omitempty" json:"tags,omitempty"`
 	Published   bool     `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
 }
@@ -30,11 +30,11 @@ type NewPodcast struct {
 // Episode is the video or audio content
 type Episode struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	PodcastID   primitive.ObjectID `bson:"podcastID,omitempty" json:"podcastID,omitempty"`
-	Title       string             `bson:"title,omitempty" json:"title,omitempty"`
-	Description string             `bson:"description,omitempty" json:"description,omitempty"`
-	Duration    int32              `bson:"duration,omitempty" json:"duration,omitempty"`
-	Spins       int                `bson:"spins,omitempty,default:0" json:"spins,omitempty,default:0`
+	PodcastID   primitive.ObjectID `bson:"podcastID,omitempty" json:"podcastID,omitempty" validate:"required"`
+	Title       string             `bson:"title,omitempty" json:"title,omitempty" validate:"required"`
+	Description string             `bson:"description,omitempty" json:"description,omitempty" validate:"required"`
+	Duration    int32              `bson:"duration,omitempty" json:"duration,omitempty" validate:"gte=0"`
+	Spins       int                `bson:"spins,omitempty,default:0" json:"spins,omitempty,default:0" validate:"gte=0"`
 	Published   bool               `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
 	Tags        []string           `bson:"tags,omitempty" json:"tags,omitempty"`
 	CreatedAt   time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
@@ -43,11 +43,11 @@ type Episode struct {
 
 // NewEpisode is the video or audio content
 type NewEpisode struct {
-	PodcastID   primitive.ObjectID `bson:"podcastID,omitempty" json:"podcastID,omitempty"`
-	Title       string             `bson:"title,omitempty" json:"title,omitempty"`
-	Description string             `bson:"description,omitempty" json:"description,omitempty"`
-	Duration    int32              `bson:"duration,omitempty" json:"duration,omitempty"`
-	Spins       int                `bson:"spins,omitempty,default:0" json:"spins,omitempty,default:0`
+	PodcastID   primitive.ObjectID `bson:"podcastID,omitempty" json:"podcastID,omitempty" validate:"required"`
+	Title       string             `bson:"title,omitempty" json:"title,omitempty" validate:"required"`
+	Description string             `bson:"description,omitempty" json:"description,omitempty" validate:"required"`
+	Duration    int32              `bson:"duration,omitempty" json:"duration,omitempty" validate:"gte=0"`
+	Spins       int                `bson:"spins,omitempty,default:0" json:"spins,omitempty,default:0" validate:"gte=0"`
 	Published   bool               `bson:"published,omitempty,default:false" json:"published,omitempty,default:false"`
 	Tags        []string           `bson:"tags,omitempty" json:"tags,omitempty"`
 }
