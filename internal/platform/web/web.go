8142d4c0c744c459f3251021c5333699fd6dd095
awe <a@awews.com>
2020-08-09 20:09:02 -0700

    create custom handler to manage other handlers
    Handle connects a method and URL pattern to a particular application handler

--- internal/platform/web/web.go
+++ internal/platform/web/web.go
@@ -7,6 +7,9 @@ import (
 	"github.com/go-chi/chi"
 )
 
+// Handler is the signature that all application handlers will implement.
+type Handler func(http.ResponseWriter, *http.Request) error
+
 // App is the entrypoint for all web applications; http package for entire project
 type App struct {
 	mux *chi.Mux
@@ -21,7 +24,18 @@ func NewApp(logger *log.Logger) *App {
 }
 
 // Handle connects a method and URL pattern to a particular application handler
-func (a *App) Handle(method, pattern string, fn http.HandlerFunc) {
+func (a *App) Handle(method, pattern string, h Handler) {
+
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		if err := h(w, r); err != nil {
+			resp := ErrorResponse{
+				Error: err.Error(),
+			}
+			if err := Respond(w, resp, http.StatusInternalServerError); err != nil {
+				a.log.Println(err)
+			}
+		}
+	}
 	a.mux.MethodFunc(method, pattern, fn)
 }
 
