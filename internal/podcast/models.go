ccdff6a1c09eda5af59a3129da2592be58f9d3a6
awe <a@awews.com>
2020-08-09 17:56:44 -0700

    add time updated/created to model

--- internal/podcast/models.go
+++ internal/podcast/models.go
@@ -1,11 +1,17 @@
 package podcast
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Podcast type is a group of related episodes
 type Podcast struct {
-	ID     primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	Title  string             `bson:"title,omitempty" json:"title,omitempty"`
-	Author string             `bson:"author,omitempty" json:"author,omitempty"`
-	Tags   []string           `bson:"tags,omitempty" json:"tags,omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Title     string             `bson:"title,omitempty" json:"title,omitempty"`
+	Author    string             `bson:"author,omitempty" json:"author,omitempty"`
+	Tags      []string           `bson:"tags,omitempty" json:"tags,omitempty"`
+	CreatedAt time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UpdatedAt time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
 }
