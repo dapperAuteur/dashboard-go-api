58ff911e87f78802992f2b574a543643eb3b8ac5
awe <a@awews.com>
2020-08-11 14:53:26 -0700

    create new struct to hold status code and time for tracking requests

--- internal/platform/web/web.go
+++ internal/platform/web/web.go
@@ -1,11 +1,25 @@
 package web
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 )
+
+// ctxKey represents the type of value for the context key.
+type ctxKey int
+
+// KeyValues is how request values or stored/retrieved.
+const KeyValues ctxKey = 1
+
+// Values carries information about each request.
+type Values struct {
+	StatusCode int
+	Start      time.Time
+}
 
 // Handler is the signature that all application handlers will implement.
 type Handler func(http.ResponseWriter, *http.Request) error
@@ -32,6 +46,15 @@ func (a *App) Handle(method, pattern string, h Handler) {
 	h = wrapMiddleware(a.mw, h)
 
 	fn := func(w http.ResponseWriter, r *http.Request) {
+
+		v := Values{
+			Start: time.Now(),
+		}
+
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, KeyValues, &v)
+		r = r.WithContext(ctx)
+
 		if err := h(w, r); err != nil {
 			a.log.Printf("ERROR : Unhandled error %v", err)
 
