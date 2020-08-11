d4eca1c0763d4100f3e046ccd53cd2ce26ffd1d9
awe <a@awews.com>
2020-08-11 12:24:03 -0700

    BUG: create function in db package to check db health
    doesn't work

--- internal/platform/database/database.go
+++ internal/platform/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -21,4 +22,20 @@ func Open(cfg Config) (*mongo.Client, error) {
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.AtlasURI))
 
 	return client, err
+}
+
+// StatusCheck returns nil if it can successfully talk to the database.
+// It returns a non-nil error otherwise.
+func StatusCheck(ctx context.Context, db *mongo.Collection) error {
+
+	// Run a simple query to determine connectivity. The db has a "Ping" method
+	// but it can false-positive when it was previously able to talk to the
+	// database but the database has since gone away. Running this query forces a
+	// round trip to the database.
+	// if err := db.Database().RunCommand(ctx, "connectionStatus"); err != nil {
+	// 	return errors.Wra
+	// }
+	connectionStatus := db.Database().RunCommand(ctx, "connectionStatus")
+	fmt.Print(connectionStatus)
+	return nil
 }
