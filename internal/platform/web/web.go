c10224b24c4e4a45229f2cc39ce098344eac5782
awe <a@awews.com>
2020-08-12 19:01:30 -0700

    refactor to use middleware to authenticate all web requests

--- internal/platform/web/web.go
+++ internal/platform/web/web.go
@@ -40,27 +40,40 @@ func NewApp(logger *log.Logger, mw ...Middleware) *App {
 	}
 }
 
-// Handle connects a method and URL pattern to a particular application handler
-func (a *App) Handle(method, pattern string, h Handler) {
+// Handle associates a handler function with an HTTP Method and URL pattern.
+//
+// It converts our custom handler type to the std lib Handler type. It captures
+// errors from the handler and serves them to the client in a uniform way.
+func (a *App) Handle(method, pattern string, h Handler, mw ...Middleware) {
 
+	// First wrap handler specific middleware around this handler.
+	h = wrapMiddleware(mw, h)
+
+	// Add the application's general middleware to the handler chain.
 	h = wrapMiddleware(a.mw, h)
 
+	// Create a function that conforms to the std lib definition of a handler.
+	// This is the first thing that will be executed when this route is called.
 	fn := func(w http.ResponseWriter, r *http.Request) {
 
+		// Create a Values struct to record state for the request. Store the
+		// address in the request's context so it is sent down the call chain.
 		v := Values{
 			Start: time.Now(),
 		}
 
 		ctx := context.WithValue(r.Context(), KeyValues, &v)
 
+		// Run the handler chain and catch any propagated error.
 		if err := h(ctx, w, r); err != nil {
-			a.log.Printf("ERROR : Unhandled error %v", err)
+			a.log.Printf("ERROR : Unhandled error %+v", err)
 
 		}
 	}
 	a.mux.MethodFunc(method, pattern, fn)
 }
 
+// ServeHTTP implements the http.Handler interface.
 func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.mux.ServeHTTP(w, r)
 }
