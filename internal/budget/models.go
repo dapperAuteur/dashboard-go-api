6235b2c2f5602a664adb556603418712bb71d2d2
awe <a@awews.com>
2020-08-15 14:20:44 -0700

    resolve issue with ID appearing in model instead of _id

--- internal/budget/models.go
+++ internal/budget/models.go
@@ -8,15 +8,15 @@ import (
 
 // Budget type is a group of related financial transactions
 type Budget struct {
-	ID          primitive.ObjectID `bson:"_id",omitempty"json:"_id,omitempty" validate:"required"`
-	BudgetName  string             `bson:"budget_name,omitempty"json:"budget_name,omitempty"validate:"required"`
-	BudgetValue float64            `bson:"budget_value,omitempty"json:"budget_value,omitempty"default:"0"`
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty" validate:"required"`
+	BudgetName  string             `bson:"budget_name,omitempty" json:"budget_name,omitempty" validate:"required"`
+	BudgetValue float64            `bson:"budget_value,omitempty" json:"budget_value,omitempty" default:"0"` // default doesn't give desired result
 	CreatedAt   time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty" validate:"datetime"`
 	UpdatedAt   time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty" validate:"datetime"`
 }
 
 // NewBudget type is what's required from the client to create a new Budget
 type NewBudget struct {
-	BudgetName  string  `bson:"budget_name,omitempty"json:"budget_name,omitempty"validate:"required"`
-	BudgetValue float64 `bson:"budget_value,omitempty"json:"budget_value,omitempty"default:"0"`
+	BudgetName  string  `bson:"budget_name,omitempty" json:"budget_name,omitempty" validate:"required"`
+	BudgetValue float64 `bson:"budget_value,omitempty" json:"budget_value,omitempty" default:"0"` // default doesn't give desired result
 }
