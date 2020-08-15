592a464a43a8082ea4d7090a29e498384af3ef16
awe <a@awews.com>
2020-08-15 14:08:20 -0700

    create service to create budget

--- internal/budget/budget.go
+++ internal/budget/budget.go
@@ -2,7 +2,10 @@ package budget
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/dapperAuteur/dashboard-go-api/internal/platform/auth"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson" // for BSON ObjectID
 	"go.mongodb.org/mongo-driver/mongo"
@@ -23,4 +26,24 @@ func List(ctx context.Context, db *mongo.Collection) ([]Budget, error) {
 
 	return list, nil
 
+}
+
+// Create adds a Budget to the database.
+// It returns the created Budget with fields like ID and CreatedAt populated.
+func Create(ctx context.Context, db *mongo.Collection, user auth.Claims, newBudget NewBudget, now time.Time) (*Budget, error) {
+
+	budget := Budget{
+		BudgetName:  newBudget.BudgetName,
+		BudgetValue: newBudget.BudgetValue,
+		CreatedAt:   now.UTC(),
+		UpdatedAt:   now.UTC(),
+	}
+
+	budgetResult, err := db.InsertOne(ctx, budget)
+	if err != nil {
+		return nil, errors.Wrapf(err, "inserting Budget: %v", newBudget)
+	}
+	fmt.Println("podcastResult : ", budgetResult)
+
+	return &budget, nil
 }
