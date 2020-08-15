a2a520e749db532f700733840774167b96f57c1b
awe <a@awews.com>
2020-08-15 14:07:10 -0700

    add handler to create budget

--- cmd/dashboard-api/internal/handlers/budget.go
+++ cmd/dashboard-api/internal/handlers/budget.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/dapperAuteur/dashboard-go-api/internal/budget"
+	"github.com/dapperAuteur/dashboard-go-api/internal/platform/auth"
 	"github.com/dapperAuteur/dashboard-go-api/internal/platform/web"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.opencensus.io/trace"
@@ -29,4 +31,26 @@ func (b Budget) List(ctx context.Context, w http.ResponseWriter, r *http.Request
 	}
 
 	return web.Respond(ctx, w, list, http.StatusOK)
+}
+
+// Create decodes the body of a request to create a new budget.
+// The full budget with generated fields is sent back in the response.
+func (b Budget) Create(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	claims, ok := ctx.Value(auth.Key).(auth.Claims)
+	if !ok {
+		return web.NewShutdownError("auth claims missing from context")
+	}
+
+	var newBudget budget.NewBudget
+
+	if err := web.Decode(r, &newBudget); err != nil {
+		return err
+	}
+
+	budget, err := budget.Create(ctx, b.DB, claims, newBudget, time.Now())
+	if err != nil {
+		return err
+	}
+
+	return web.Respond(ctx, w, budget, http.StatusCreated)
 }
