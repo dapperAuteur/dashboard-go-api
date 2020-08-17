b4a0190ed4faa05fc6dd3daa8d74b33ffb81f828
awe <a@awews.com>
2020-08-16 19:59:11 -0700

    create handler to create vendor

--- cmd/dashboard-api/internal/handlers/vendor.go
+++ cmd/dashboard-api/internal/handlers/vendor.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/dapperAuteur/dashboard-go-api/internal/apierror"
 	"github.com/dapperAuteur/dashboard-go-api/internal/budget"
+	"github.com/dapperAuteur/dashboard-go-api/internal/platform/auth"
 	"github.com/dapperAuteur/dashboard-go-api/internal/platform/web"
+	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.opencensus.io/trace"
 )
@@ -30,4 +34,31 @@ func (v Vendor) ListVendors(ctx context.Context, w http.ResponseWriter, r *http.
 	}
 
 	return web.Respond(ctx, w, list, http.StatusOK)
+}
+
+// CreateVendor decodes the body of a reuqest to create a new vendor.
+// The full vendor with generated fields is sent back in the response.
+func (v Vendor) CreateVendor(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+	claims, ok := ctx.Value(auth.Key).(auth.Claims)
+	if !ok {
+		return web.NewShutdownError("auth claims missing from context")
+	}
+
+	var newVendor budget.NewVendor
+
+	if err := web.Decode(r, &newVendor); err != nil {
+		return err
+	}
+
+	vendorCreated, err := budget.CreateVendor(ctx, v.DB, claims, newVendor, time.Now())
+	if err != nil {
+		switch err {
+		case apierror.ErrForbidden:
+			return web.NewRequestError(err, http.StatusForbidden)
+		default:
+			return errors.Wrapf(err, "creating vendor %q", newVendor)
+		}
+	}
+	return web.Respond(ctx, w, vendorCreated, http.StatusCreated)
 }
