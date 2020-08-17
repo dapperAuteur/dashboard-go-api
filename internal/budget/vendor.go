0c6fd946ff71eaee3a8b44eb661aeb98da2a2499
awe <a@awews.com>
2020-08-16 19:43:29 -0700

    create service to list vendors

---
+++ internal/budget/vendor.go
@@ -0,0 +1,55 @@
+package budget
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/dapperAuteur/dashboard-go-api/internal/apierror"
+	"github.com/dapperAuteur/dashboard-go-api/internal/platform/auth"
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// ListVendors gets all the Vendors from the db then encodes them in a response client
+func ListVendors(ctx context.Context, db *mongo.Collection) ([]Vendor, error) {
+
+	list := []Vendor{}
+
+	cursor, err := db.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "getting cursor from vendor collection.")
+	}
+
+	if err = cursor.All(ctx, &list); err != nil {
+		return nil, errors.Wrapf(err, "retrieving vendor list")
+	}
+
+	return list, nil
+}
+
+// CreateVendor takes data from the client to create a vendor in the db
+func CreateVendor(ctx context.Context, db *mongo.Collection, user auth.Claims, newVendor NewVendor, now time.Time) (*Vendor, error) {
+
+	var isAdmin = user.HasRole(auth.RoleAdmin)
+
+	if !isAdmin {
+		return nil, apierror.ErrForbidden
+	}
+
+	vendor := Vendor{
+		VendorName: newVendor.VendorName,
+		CreatedAt:  now.UTC(),
+		UpdatedAt:  now.UTC(),
+	}
+
+	vResult, err := db.InsertOne(ctx, vendor)
+	if err != nil {
+		return nil, errors.Wrapf(err, "inserting vendor : %v", vendor)
+	}
+
+	fmt.Println("vResult : ", vResult)
+
+	return &vendor, nil
+}
