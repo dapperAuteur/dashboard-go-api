812d2a4236bbb270c7eb13cc24b73a2b0d53effd
awe <a@awews.com>
2020-08-17 13:39:04 -0700

    add route to get transaction by _id

--- cmd/dashboard-api/internal/handlers/routes.go
+++ cmd/dashboard-api/internal/handlers/routes.go
@@ -85,6 +85,7 @@ func API(shutdown chan os.Signal, logger *log.Logger, db *mongo.Database, authen
 	// Transaction Routes
 	app.Handle(http.MethodGet, "/v1/transactions", transaction.ListTransactions)
 	app.Handle(http.MethodPost, "/v1/transactions", transaction.CreateTransaction, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
+	app.Handle(http.MethodGet, "/v1/transactions/{_id}", transaction.RetrieveTransaction)
 
 	// Vendor Routes
 	app.Handle(http.MethodGet, "/v1/vendors", vendor.ListVendors)
