58a17f80988fcd2cc9ff97bb23ce528f3deac11b
awe <a@awews.com>
2020-08-18 19:47:46 -0700

    add route to update one Note

--- cmd/dashboard-api/internal/handlers/routes.go
+++ cmd/dashboard-api/internal/handlers/routes.go
@@ -95,6 +95,7 @@ func API(shutdown chan os.Signal, logger *log.Logger, db *mongo.Database, authen
 	app.Handle(http.MethodGet, "/v1/notes", note.ListNotes)
 	app.Handle(http.MethodPost, "/v1/notes", note.CreateNote, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
 	app.Handle(http.MethodGet, "/v1/notes/{_id}", note.RetrieveNote)
+	app.Handle(http.MethodPut, "/v1/notes/{_id}", note.UpdateOneNote, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
 
 	// Transaction Routes
 	app.Handle(http.MethodGet, "/v1/transactions", transaction.ListTransactions)
