8e4fcd159c24310adc3541d074a01dcc7f32d108
awe <a@awews.com>
2020-08-18 19:10:03 -0700

    add service to create Note

--- internal/blog/note.go
+++ internal/blog/note.go
@@ -2,7 +2,12 @@ package blog
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/dapperAuteur/dashboard-go-api/internal/apierror"
+	"github.com/dapperAuteur/dashboard-go-api/internal/platform/auth"
+	"github.com/dapperAuteur/dashboard-go-api/internal/utility"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -22,4 +27,37 @@ func ListNotes(ctx context.Context, db *mongo.Collection) ([]Note, error) {
 		return nil, errors.Wrapf(err, "retrieving note list")
 	}
 	return list, nil
+}
+
+// CreateNote takes data from the client to create a note in the db
+func CreateNote(ctx context.Context, db *mongo.Collection, user auth.Claims, newNote NewNote, now time.Time) (*Note, error) {
+
+	var isAdmin = user.HasRole(auth.RoleAdmin)
+
+	if !isAdmin {
+		return nil, apierror.ErrForbidden
+	}
+
+	var strSlice []string
+	// check if prop is provided
+	if newNote.PropertyAssociation != nil {
+		strSlice = append(newNote.PropertyAssociation, newNote.PropertyAssociation...)
+		strSlice = utility.RemoveDuplicateStringValues(strSlice)
+	}
+
+	note := Note{
+		NoteText:            newNote.NoteText,
+		PropertyAssociation: strSlice,
+		CreatedAt:           now.UTC(),
+		UpdatedAt:           now.UTC(),
+	}
+
+	nResult, err := db.InsertOne(ctx, note)
+	if err != nil {
+		return nil, errors.Wrapf(err, "inserting note : %v", note)
+	}
+
+	fmt.Println("nResult : ", nResult)
+
+	return &note, nil
 }
