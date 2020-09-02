c598e37bd39f2b2529a71ff33af7366d01d5c47a
awe <a@awews.com>
2020-09-02 13:20:55 -0700

    add models for Word, NewWord, and UpdateWord

---
+++ internal/word/models.go
@@ -0,0 +1,50 @@
+package word
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Word type is a group of English words
+type Word struct {
+	ID               primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty" validate:"required"`
+	Meaning          []string           `bson:"meaning,omitempty" json:"meaning,omitempty"`
+	Tongue           string             `bson:"tongue,omitempty" json:"tongue,omitempty"`
+	Tier             int                `bson:"tier,omitempty" json:"tier,omitempty"`
+	Word             string             `bson:"word,omitempty" json:"word,omitempty"`
+	InGame           bool               `bson:"in_game,omitempty" json:"in_game,omitempty" default:"false"`
+	SPoints          int                `bson:"s_points,omitempty" json:"s_points,omitempty"`
+	FPoints          int                `bson:"f_points,omitempty" json:"f_points,omitempty"`
+	IsFourLetterWord bool               `bson:"is_four_letter_word,omitempty" json:"is_four_letter_word,omitempty"`
+	CreatedAt        time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty" validate:"datetime"`
+	UpdatedAt        time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty" validate:"datetime"`
+}
+
+// NewWord type is what's required from the client to create a new Word
+type NewWord struct {
+	Meaning          []string `bson:"meaning,omitempty" json:"meaning,omitempty"`
+	Tongue           string   `bson:"tongue,omitempty" json:"tongue,omitempty"`
+	Tier             int      `bson:"tier,omitempty" json:"tier,omitempty"`
+	Word             string   `bson:"word,omitempty" json:"word,omitempty"`
+	InGame           bool     `bson:"in_game,omitempty" json:"in_game,omitempty" default:"false"`
+	SPoints          int      `bson:"s_points,omitempty" json:"s_points,omitempty"`
+	FPoints          int      `bson:"f_points,omitempty" json:"f_points,omitempty"`
+	IsFourLetterWord bool     `bson:"is_four_letter_word,omitempty" json:"is_four_letter_word,omitempty"`
+}
+
+// UpdateWord defines what information may be provided to modify an existing Word.
+// All fields are optional so the clients can send just the fields they want changed.
+// It uses pointer fields so we can differentiate between a field that was NOT provided and a field that was provided as explicitly blank.
+// Normally we do NOT want to use pointers to basic types but we make exceptions around marshalling/unmarshalling.
+type UpdateWord struct {
+	ID               primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Meaning          *[]string          `bson:"meaning,omitempty" json:"meaning,omitempty"`
+	Tongue           *string            `bson:"tongue,omitempty" json:"tongue,omitempty"`
+	Tier             *int               `bson:"tier,omitempty" json:"tier,omitempty"`
+	Word             *string            `bson:"word,omitempty" json:"word,omitempty"`
+	InGame           *bool              `bson:"in_game,omitempty" json:"in_game,omitempty" default:"false"`
+	SPoints          *int               `bson:"s_points,omitempty" json:"s_points,omitempty"`
+	FPoints          *int               `bson:"f_points,omitempty" json:"f_points,omitempty"`
+	IsFourLetterWord *bool              `bson:"is_four_letter_word,omitempty" json:"is_four_letter_word,omitempty"`
+}
