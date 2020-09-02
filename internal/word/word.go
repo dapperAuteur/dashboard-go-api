9ee8842920361b1e64861b7a159974991892a3b8
awe <a@awews.com>
2020-09-02 14:54:42 -0700

    add service to get word by _id

--- internal/word/word.go
+++ internal/word/word.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/dapperAuteur/dashboard-go-api/internal/apierror"
 	"github.com/dapperAuteur/dashboard-go-api/internal/platform/auth"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -28,7 +30,43 @@ func WordList(ctx context.Context, db *mongo.Collection) ([]Word, error) {
 	return wordList, nil
 }
 
-// RetrieveWord gets the first Word in the db with the provided _id
+// RetrieveWordByID gets the first Word in the db with the provided _id
+func RetrieveWordByID(ctx context.Context, db *mongo.Collection, _id string) (*Word, error) {
+
+	var word Word
+
+	id, err := primitive.ObjectIDFromHex(_id)
+	if err != nil {
+		return nil, apierror.ErrInvalidID
+	}
+
+	if err := db.FindOne(ctx, bson.M{"_id": id}).Decode(&word); err != nil {
+		return nil, apierror.ErrNotFound
+	}
+
+	fmt.Println("word found : ", word)
+
+	return &word, nil
+}
+
+// RetrieveWord gets the first Word in the db with the provided wd string
+func RetrieveWord(ctx context.Context, db *mongo.Collection, wd string) (*Word, error) {
+
+	var word Word
+
+	// Create filter to find word by word parameter
+	filter := Word{
+		Word: wd,
+	}
+
+	if err := db.FindOne(ctx, filter).Decode(&word); err != nil {
+		return nil, apierror.ErrNotFound
+	}
+
+	fmt.Println("word found : ", word)
+
+	return &word, nil
+}
 
 // CreateWord adds a Word to the database.
 // It returns the created Word with fields populated, NOT the ID field tho'. FIX LATER.
