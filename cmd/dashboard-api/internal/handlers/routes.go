e096d746ac8d662853ebd9fad7578ef9df57b60d
awe <a@awews.com>
2020-09-02 20:07:39 -0700

    add route to delete affix by _id

--- cmd/dashboard-api/internal/handlers/routes.go
+++ cmd/dashboard-api/internal/handlers/routes.go
@@ -156,6 +156,7 @@ func API(shutdown chan os.Signal, logger *log.Logger, db *mongo.Database, authen
 	app.Handle(http.MethodPost, "/v1/affixes", affix.CreateAffix, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
 	app.Handle(http.MethodGet, "/v1/affixes/{_id}", affix.RetrieveAffixByID)
 	app.Handle(http.MethodPut, "/v1/affixes/{_id}", affix.UpdateOneAffix, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
+	app.Handle(http.MethodDelete, "/v1/affixes/{_id}", affix.DeleteAffixByID, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
 
 	return app
 }
