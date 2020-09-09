1232c58090dda12c63342a3fa9bb010acdfa653f
awe <a@awews.com>
2020-09-09 16:28:23 -0700

    add service to get currency list

---
+++ internal/budget/currency.go
@@ -0,0 +1,26 @@
+package budget
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// CurrencyList gets all the currencies from the database then encodes them in a response client.
+func CurrencyList(ctx context.Context, db *mongo.Collection) ([]Currency, error) {
+
+	currencyList := []Currency{}
+
+	currencyCursor, err := db.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "getting currencyCursor retrieving currency list")
+	}
+
+	if err = currencyCursor.All(ctx, &currencyList); err != nil {
+		return nil, errors.Wrapf(err, "retrieving currency list")
+	}
+
+	return currencyList, nil
+}
