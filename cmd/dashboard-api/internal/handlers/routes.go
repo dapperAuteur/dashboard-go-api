68923b2bb38912f270a5dcdc02faf58e5afda0fb
awe <a@awews.com>
2021-04-10 19:32:11 -0700

    comment out filter tranx handler route

--- cmd/dashboard-api/internal/handlers/routes.go
+++ cmd/dashboard-api/internal/handlers/routes.go
@@ -135,7 +135,7 @@ func API(shutdown chan os.Signal, logger *log.Logger, db *mongo.Database, authen
 
 	// Transaction Routes
 	app.Handle(http.MethodGet, "/v1/transactions", transaction.ListTransactions)
-	// app.Handle(http.MethodGet, "/v1/transactions/filter", transaction.FilterTransactions)
+	// app.Handle(http.MethodPost, "/v1/transactions/filter", transaction.FilterTransactions)
 	app.Handle(http.MethodPost, "/v1/transactions", transaction.CreateTransaction, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
 	app.Handle(http.MethodGet, "/v1/transactions/{_id}", transaction.RetrieveTransaction)
 	app.Handle(http.MethodPut, "/v1/transactions/{_id}", transaction.UpdateOneTransaction, mid.Authenticate(authenticator), mid.HasRole(auth.RoleAdmin))
